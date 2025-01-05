ffb351097aa244f62c34a221bb634590887cedd3
oswaldojmp <oswaldojesusmarinpages@gmail.com>
2025-01-05 16:09:48 -0400

    Agregando archivos al Repositorio
    Agregando archivos al repositorio Git Local

---
+++ Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnContra = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtContra = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnContra
+        '
+        Me.btnContra.BackColor = System.Drawing.Color.Maroon
+        Me.btnContra.Font = New System.Drawing.Font("Constantia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnContra.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnContra.Location = New System.Drawing.Point(122, 83)
+        Me.btnContra.Name = "btnContra"
+        Me.btnContra.Size = New System.Drawing.Size(182, 68)
+        Me.btnContra.TabIndex = 0
+        Me.btnContra.Text = "Probar Contrasena"
+        Me.btnContra.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Constantia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(399, 19)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Usted tiene 15 segundos para escribir su contrasena"
+        '
+        'txtContra
+        '
+        Me.txtContra.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtContra.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtContra.Font = New System.Drawing.Font("Constantia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtContra.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.txtContra.Location = New System.Drawing.Point(16, 50)
+        Me.txtContra.Name = "txtContra"
+        Me.txtContra.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtContra.Size = New System.Drawing.Size(395, 27)
+        Me.txtContra.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 15000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(426, 162)
+        Me.Controls.Add(Me.txtContra)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnContra)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Contrasena"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnContra As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtContra As TextBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        MsgBox("Lo siento, se acabó su tiempo.")
+        End
+    End Sub
+
+    Private Sub btnContra_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnContra.Click
+        If txtContra.Text = "secreta" Then
+            Timer1.Enabled = False
+            MsgBox("¡Bienvenido al sistema!")
+            End
+        Else
+            MsgBox("Lo siento, amigo. No sé quién es usted.")
+        End If
+    End Sub
+End Class
